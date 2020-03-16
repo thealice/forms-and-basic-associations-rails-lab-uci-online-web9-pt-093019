b4881671d9b482d99aebe252769534b6efe707c5
Alice Freda <alicefreda@gmail.com>
2020-03-16 16:51:48 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
-  # add associations here
+ belongs_to :artist
+ belongs_to :genre
+ has_many :notes
+
+ accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
+
+ def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+ end
+
+ def artist_name
+  self.artist ? self.artist.name : nil
+ end
+
+ def genre_name=(name)
+  self.genre = Genre.find_or_create_by(name: name)
+ end
+
+ def genre_name
+  self.genre ? self.genre.name : nil
+ end
+
 end
